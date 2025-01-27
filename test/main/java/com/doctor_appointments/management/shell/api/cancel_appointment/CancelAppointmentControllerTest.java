0e1e8b8237772e1452ce5e41096cb141e261c94a
AhmedSedek <ahmed.magdy.g.s@gmail.com>
2025-01-27 20:53:00 +0000

    Add empty tests for Management module controllers

---
+++ test/main/java/com/doctor_appointments/management/shell/api/cancel_appointment/CancelAppointmentControllerTest.java
@@ -0,0 +1,7 @@
+package main.java.com.doctor_appointments.management.shell.api.cancel_appointment;
+
+/*
+ * The Management module controllers are just proxying all requests to ManagementService.
+ * Tests are irrelevant at the moment. Keeping the file empty for now.
+*/
+public class CancelAppointmentControllerTest {}
---
+++ test/main/java/com/doctor_appointments/management/shell/api/complete_appointment/CompleteAppointmentControllerTest.java
@@ -0,0 +1,7 @@
+package main.java.com.doctor_appointments.management.shell.api.complete_appointment;
+
+/*
+ * The Management module controllers are just proxying all requests to ManagementService.
+ * Tests are irrelevant at the moment. Keeping the file empty for now.
+ */
+public class CompleteAppointmentControllerTest {}
